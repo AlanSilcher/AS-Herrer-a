492e102838b6983681c4c29197a5a6c9b16a0689
AlanSilcher <silcheralan@gmail.com>
2024-05-09 17:43:05 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React from 'react';
+import Home from './Components/Home/Home';
+import Data from './Components/Data/Data';
+import Projects from './Components/Projects/Projects';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <h1>Primer paso</h1>
-    </div>
+      <div className="App">
+        <h1>sss</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/data" element={<Data />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </div>
   );
 }
 
 export default App;
+
+
+
+
---
+++ src/Components/Data/Data.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Data() {
+  return (
+
+    <div>
+      <h1>Data Page</h1>
+    </div>
+  
+  );
+}
+
+export default Data;
